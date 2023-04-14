1628ac9b4993c042185f9d9d489d87c2a5829e8a
LebranD <xianshundeng@gmail.com>
2023-04-14 14:38:19 +0800

    Initial Commit

---
+++ STTDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STTDemo
+//
+//  Created by 邓先舜 on 2023/4/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
